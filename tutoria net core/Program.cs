5139ea1343111f0f04b6ba24d5e5204543da818b
neiser.montano <neisermontano1991@gmail.com>
2022-01-06 14:48:09 -0500

    Agregar archivos de proyecto.

---
+++ tutoria net core/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutoria_net_core
+{
+    public class Program
+    {
+        // para iniciar la aplicacion,
+        //map para mapear las rutas
+        //use para ir pasando peticiones 
+
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
